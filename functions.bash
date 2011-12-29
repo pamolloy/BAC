11fe4696cfcd457388bf483c6bce36ad17250c3c
Philip Molloy <philip.a.molloy@gmail.com>
2011-12-29 14:59:57 -0500

    Remove old MP3, WAV, WMA files
    After converting the audio files, remove the files with MP3, WAV and WMA
     encoding.

--- functions.bash
+++ functions.bash
@@ -2,11 +2,16 @@
 #
 #   functions.bash - Various functions to be sourced by bac.bash
 #
+# TODO
+#   - Test each file before removing it
 
 wav_flac ()
 {
     flac --keep-foreign-metadata --best --force --silent "$1".wav
-    flac --test "$1".flac
+    if [ flac --test "$1".flac ]
+    then
+        rm "$1".wav
+    fi
 }
 
 ogg_encode ()
@@ -17,12 +22,16 @@ ogg_encode ()
 mp3_ogg ()
 {
     lame --decode "$1".mp3
+    rm "$1".mp3
     ogg_encode "$1".wav
+    rm "$1".wav
 }
 
 wma_ogg ()
 {
     mplayer -quiet -ao pcm:file="$1".wav -vc null -vo null "$1".wma
+    rm "$1".wma
     ogg_encode "$1".wav
+    rm "$1".wav
 }
 
