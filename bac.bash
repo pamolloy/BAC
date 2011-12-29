93e3ad8fc7c0951252eb763c6bb2f5b7cc6f6de2
Philip Molloy <philip.a.molloy@gmail.com>
2011-12-29 00:07:29 -0500

    Add new functions
    Create functions to check dependencies, search directory recursively, and
     convert files with WAV, MP3, WMA extensions.

--- bac.bash
+++ bac.bash
@@ -6,92 +6,75 @@
 # Load data files
 . lang.bash         # Language variables
 . functions.bash    # Various functions
-. convert.bash      # Function to convert audio files
 
 # Exit if no arguments provided
 if [ $# = 0 ]
 then
-    local FILE=$1
-else
     echo ${NO_ARGS[2]}
     exit ${NO_ARGS[1]}
 fi
 
-# Dependency check
-DEPS = ( 0 0 0 0 0 0 )
-case "${FILE##*.}" in
-    mp3)
-        if which lame 2>/dev/null
-            DEPS[1]=1
-        else
-            echo ${NO_DEP[2]}
-            exit ${NO_DEP[1]}
-        ;;
-    ogg)
-        if which oggenc 2>/dev/null
-            DEPS[2]=1
-        else
-            echo ${NO_DEP[2]}
-            exit ${NO_DEP[1]}
-        ;;
-    mpc)
-        if which mppenc 2>/dev/null
-            DEPS[3]=1
-        else
-            echo ${NO_DEP[2]}
-            exit ${NO_DEP[1]}
-        ;;
-    flac)
-        if which flac 2>/dev/null
-            DEPS[4]=1
-        else
-            echo ${NO_DEP[2]}
-            exit ${NO_DEP[1]}
-        ;;
-    ape)
-        if which mac 2>/dev/null
-            DEPS[5]=1
-        else
-            echo ${NO_DEP[2]}
-            exit ${NO_DEP[1]}
-        ;;
-    wav)
-        DEPS[6]=1
-        ;;
-esac
-
-## Options - prompt user for input
-ENCODE= #TODO (PM) Store user input in variable
-
-# Output filename
-BASENAME="${FILE%.*}"
-FILENAME="$BASENAME.$EXTENSION" #TODO (PM) Create variable
-if [ -e "$FILENAME" ] # Check existence of output filename in PWD
-then
-    echo ${FILE_EXIST[2]}
-    #TODO (PM) Prompt user to overwrite file
-    if #TODO (PM) If user does NOT want to overwrite
-        exit ${FILE_EXIST[1]}
-fi
-
-# Meta data conversion for MP3, OGG, or FLAC codecs
-for CODEC in 1 2 4
-do
-    if [ "${DEPS[CODEC]}" == 1 ]
+depchk ()
+{
+    if ! which $1 2>/dev/null
     then
-        echo $META_DATA
-        # Store user input
+        echo "${NO_DEP[2]}" "$1"
+        exit ${NO_DEP[1]}
     fi
+}
+
+search ()
+{
+    DIR=$1
+    
+    for FILE in `ls DIR`
+    do
+        if [ -d "$FILE" ]
+        then
+            # Check if file is a symbolic link
+            if [ -L "$FILE" ]
+            then
+                echo ${SYM[2]}
+                exit ${SYM[1]}
+            else
+                # If file is a directory, search recursively
+                search $FILE
+            fi
+        else
+            # If file is a regular file convert it
+            if [ -f "$FILE" ]
+            then
+                convert $FILE
+            fi
+        fi
+    done
+}
+
+convert ()
+{
+    FILE=$1
+    BASE="${FILE%.*}"
+
+    case "${FILE##*.}" in
+        mp3)
+            mp3_ogg $BASE
+            ;;
+        wma)
+            wma_ogg $BASE
+            ;;
+        wav)
+            wav_flac $BASE
+            ;;
+    esac
+}
+
+# Dependency check
+for APP in lame oggenc flac mplayer
+do
+    depchk $APP
 done
 
-# Compression quality
-if [ "${DEPS[6]}" == 1 ]
-then
-    echo $QUALITY
-    # Store user input
-fi
-
-## Convert audio file
-caf "$in_file" "$out_file" "$formatout"
+# Search input directory
+search $1
 
 exit 0
