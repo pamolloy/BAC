4fbf5663ff874e7f1449479628e2a70a23b07c0d
Philip Molloy <philip.a.molloy@gmail.com>
2011-12-29 12:48:56 -0500

    Add command options, rename functions
    The calls to the functions were renamed in bac.bash, but not in the functions
     file. Add --silence and --quiet options to commands.

--- functions.bash
+++ functions.bash
@@ -3,30 +3,26 @@
 #   functions.bash - Various functions to be sourced by bac.bash
 #
 
-wav-flac ()
+wav_flac ()
 {
-    flac "$1".wav --keep-foreign-metadata --best
+    flac --keep-foreign-metadata --best --force --silent "$1".wav
     flac --test "$1".flac
 }
 
-ogg-encode ()
+ogg_encode ()
 {
-    oggenc --quality 5 $1
+    oggenc --quality 5 "$1"
 }
 
-mp3-ogg ()
+mp3_ogg ()
 {
     lame --decode "$1".mp3
-    ogg-encode "$1".wav
+    ogg_encode "$1".wav
 }
 
 wma_ogg ()
 {
-    mplayer -ao pcm:file="$1".wav -vc null -vo null
-    ogg-encode "$1".wav
+    mplayer -quiet -ao pcm:file="$1".wav -vc null -vo null "$1".wma
+    ogg_encode "$1".wav
 }
 
-aac_ogg ()
-{
-    #TODO (PM) Add AAC conversion
-}
